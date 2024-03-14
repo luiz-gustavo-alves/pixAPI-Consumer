b226f48f8b8e5cf5b63a15fd8b1799e35219855e
Luiz Gustavo <luizgustavoalves702@gmail.com>
2024-03-14 16:52:38 -0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new() 
+{
+  HostName = "localhost"
+};
+
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+  queue: "payments",
+  durable: false,
+  exclusive: false,
+  autoDelete: false,
+  arguments: null  
+);
+
+EventingBasicConsumer consumer = new(channel);
+
+Console.WriteLine("[*] Waiting for new messages");
+consumer.Received += async(model, ea) => 
+{
+  var body = ea.Body.ToArray();
+  var message = Encoding.UTF8.GetString(body);
+  Console.WriteLine($"{message}");
+};
+
+channel.BasicConsume(
+  queue: "payments",
+  autoAck: true,
+  consumer: consumer
+);
+
+Console.ReadLine();
